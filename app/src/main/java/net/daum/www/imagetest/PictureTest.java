82a53cea7b66ab30dd0102925d88f32351c7e6be
thsjikyeong <thswlrud0@naver.com>
2017-09-15 15:13:03 +0900

    hello test!

--- app/src/main/java/net/daum/www/imagetest/PictureTest.java
+++ app/src/main/java/net/daum/www/imagetest/PictureTest.java
@@ -26,8 +26,7 @@ import android.widget.Toast;
         //앨범으로 전송시 돌려받을 번호
         static int REQUEST_PHOTO_ALBUM=2;
         //첫번째 이미지 아이콘 샘플 이다.
-        static String SAMPLEIMG="ic_launcher.png";
-
+asdfasdfasdf
         static String Hello;
         ImageView iv;
       //  Dialog dialog;
